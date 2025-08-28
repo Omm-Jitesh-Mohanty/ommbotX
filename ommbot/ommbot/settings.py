38342759734eb7684414056f09197af944c90ab6
Omm Jitesh Mohanty <ommjiteshmohanty@gmail.com>
2025-08-28 19:08:36 +0530

    Update settings.py

--- ommbot/ommbot/settings.py
+++ ommbot/ommbot/settings.py
@@ -70,14 +70,14 @@ WSGI_APPLICATION = "ommbot.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+'''
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -122,5 +122,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 
 
