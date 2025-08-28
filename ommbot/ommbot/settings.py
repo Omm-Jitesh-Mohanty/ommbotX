0b841b278a00c42ffe6e89ea0a6fbb1012cb2331
Omm Jitesh Mohanty <ommjiteshmohanty@gmail.com>
2025-08-28 21:47:40 +0530

    Update settings.py

--- ommbot/ommbot/settings.py
+++ ommbot/ommbot/settings.py
@@ -77,18 +77,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-'''
-DATABASES = [
-    'default':{
-        'ENGINE':'django.db.backends.postgresql',
-        'NAME':'',
-        'USER':'',
-        'PASSWORD':'',
-        'HOST':'',
-        'PORT':'',
-    }
-]
-'''
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -130,6 +119,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 
 
 
