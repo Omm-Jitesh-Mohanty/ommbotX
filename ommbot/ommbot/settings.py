4129596113e92cfe09b907dd68a79c4bfe9fb184
Omm Jitesh Mohanty <ommjiteshmohanty@gmail.com>
2025-08-28 19:05:15 +0530

    Update settings.py

--- ommbot/ommbot/settings.py
+++ ommbot/ommbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-7(-n233z)+^egs+*$4tm@268dexlm73talb$_547cyppkm0x&e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -114,8 +114,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
